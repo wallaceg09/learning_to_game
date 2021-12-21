d33d359796e8f94e08f3d61b5cc33f71ac300bcf
Glen Wallace <wallaceglenw@gmail.com>
2021-12-21 11:49:29 -0600

    Fix dependency issues

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM tensorflow/tensorflow:2.7.0-gpu
 
 WORKDIR app
 COPY requirements.txt requirements.txt
+
+RUN apt-get update && apt-get install -y python-opengl ffmpeg xvfb python-opengl
 RUN cat requirements.txt | grep -v "tensor\|keras" > requirements_mod.txt && \
     pip install -r requirements_mod.txt
 
--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-import gym
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    import tensorflow as tf
+import uuid
 
-    print("TensorFlow version:", tf.__version__)
-    pass
+import gym
+from gym import wrappers
+from pyvirtualdisplay import Display
+
+if __name__ == '__main__':
+    virtual_display = Display(visible=False, size=(1400, 900))
+    virtual_display.start()
+
+    env = gym.make('CartPole-v0')
+    env = wrappers.Monitor(env, f"tmp/{uuid.uuid4()}")
+
+    try:
+        env.reset()
+        for _ in range(1000):
+            env.render()
+            env.step(env.action_space.sample())  # take a random action
+    finally:
+        env.close()
+        print("Finished")
